e714513cd8cc6a87c718c828a5296eba90287f88
TokiSyt <yuutokei30@gmail.com>
2025-06-08 11:01:58 +0200

    Rest Framework View & Response

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,15 +1,34 @@
 import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+
+# from django.http import JsonResponse
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from products.models import Product
 
+""" Manual Way
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
         return JsonResponse(data)
+        
         # model instance (model_data)
         # turn into a python dict
         # return Json to client
+"""
+
+
+@api_view(["GET"])  # choosing method to allow only what we want to
+def api_home(request, *args, **kwargs):
+    """
+    DRF API VIEW
+    """
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(data, fields=["id", "title", "price"])
+    return Response(data)
