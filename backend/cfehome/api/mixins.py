edac31e282f11a172b48a7181eab781f3ed63712
TokiSyt <yuutokei30@gmail.com>
2025-07-09 11:08:15 +0200

    Mixin personal note

--- backend/cfehome/api/mixins.py
+++ backend/cfehome/api/mixins.py
@@ -1,5 +1,7 @@
 from .permissions import IsStaffEditorPermission
 from rest_framework import permissions
 
+#Mixins are good to add anything to the views/shortcut the information provided to it 
+
 class StaffEditorPermissionMixin():
     permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
