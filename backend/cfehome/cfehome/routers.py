4b0d4e6737c887410a5e249cd3554940ccc884f0
TokiSyt <yuutokei30@gmail.com>
2025-07-09 11:58:33 +0200

    Reverse URL's in Serializers

--- backend/cfehome/cfehome/routers.py
+++ backend/cfehome/cfehome/routers.py
@@ -1,9 +1,9 @@
 # usually stays in urls.py
 
 from rest_framework.routers import DefaultRouter
-from products.viewsets import ProductGenericViewSet
+from products.viewsets import ProductViewSet
 
 router = DefaultRouter()
-router.register("products-list", ProductGenericViewSet, basename="products")
+router.register("products-list", ProductViewSet, basename="products")
 
 urlpatterns = router.urls
--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Product
 
 
@@ -6,10 +7,16 @@ class ProductSerializer(serializers.ModelSerializer):
 
     my_discount = serializers.SerializerMethodField(read_only=True)
     sale_event = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(  # HyperlinkedIdentityField only works in a Model Serializer and SerializerMethodField works anywhere in Serializers
+        view_name="product-detail", lookup_field="pk"
+    )
 
     class Meta:
         model = Product
         fields = [
+            "url",
+            "edit_url",
             "pk",
             "title",
             "content",
@@ -19,7 +26,13 @@ class ProductSerializer(serializers.ModelSerializer):
             "sale_event",
         ]
 
-    def get_my_discount(self, obj):  # to change the representation
+    def get_edit_url(self, obj):
+        request = self.context.get("request")  # self.request
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
+
+    def get_my_discount(self, obj):  # to change the representati on
         # obj.user -> user.username
         # obj.category -> category.etc
         if not hasattr(obj, "id") and not isinstance(obj, Product):
